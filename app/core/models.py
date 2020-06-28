9596f287a887e92083d3ca2f422e86989b266819
wissam <wessameljurdi@gmail.com>
2020-06-28 23:20:55 +0300

    minor modifications

--- app/core/models.py
+++ app/core/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
-    PermissionsMixin
+                                        PermissionsMixin
 
 
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
-        """Creares and saves a new user"""
+        """Creates and saves a new user"""
         if not email:
             raise ValueError('Users must have an email address')
         user = self.model(email=self.normalize_email(email), **extra_fields)
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -6,7 +6,7 @@ class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
         """Test creating a new user with an email is successful"""
-        email = 'test@admin.com'
+        email = 'test@user.com'
         password = 'testpass123'
         user = get_user_model().objects.create_user(
             email=email,
@@ -17,7 +17,7 @@ class ModelTests(TestCase):
         self.assertTrue(user.check_password(password))
 
     def test_new_user_email_normalized(self):
-        """Test the email for a new user is normalized"""
+        """Test if the email for a new user is normalized"""
         email = 'test@ADMIN.COM'
         user = get_user_model().objects.create_user(email, 'test123')
 
